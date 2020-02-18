0870003943d6cde09ce193fa10df1b1bc9569b6f
malashenok <a.malashenok@inbox.ru>
2020-02-18 10:51:14 +0300

    Lesson_4

--- Lesson_4/app.js
+++ Lesson_4/app.js
@@ -8,68 +8,56 @@ const consolidate = require('consolidate');
 const path = require('path');
 
 const port = 4000;
-const options = {
-    url: 'https://lenta.ru/rubrics/science/',
-};
 
 let news = {
     quantity: 10,
     content: [],
 };
 
-function getNews(err, response, body) {
-    if (!err && response.statusCode == 200) {
+function getNews(res) {
+    request('https://lenta.ru/rubrics/science/', (err, resp, body) => {
+        if (!err && resp.statusCode == 200) {
+            news.content.length = 0;
+            const $ = cheerio.load(body);
 
-        news.content.length = 0;
-        const $ = cheerio.load(body);
+            $('.item.news.b-tabloid__topic_news').each((i, item) => {
+                news.content.push($(item).find('.titles').eq(0).text());
+            });
 
-        $('.item.news.b-tabloid__topic_news').each((i, item) => {
-            news.content.push($(item).find('.titles').eq(0).text());
-        });
-
-        news.content.length = news.quantity;
-    }
+            news.content.length = news.quantity;
+            res.render('news', news);
+        } else {
+            console.log('Error while loading page');
+            res.render('error');
+        }
+    });
 }
 
 app.engine('hbs', consolidate.handlebars);
 app.set('view engine', 'hbs');
 app.set('views', path.resolve(__dirname, 'views'));
 
-app.use(express.urlencoded({
-    extended: false
-}));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-
-    request(options, getNews);
-
-    if (news.content.length > 0) {
-            res.render('news', news);
-    } else {
-            res.render('error');
-    }
+    getNews(res);
 });
 
 app.post('/', (req, res) => {
 
     let num = parseInt(req.body.quantity);
-    if (!isNaN(num)) {
+    if (!isNaN(num) && num > 0) {
         news.quantity = num;
-    } 
+    }
 
-    if(news.content.length > news.quantity) {
+    if (news.quantity < news.content.length) {
         news.content.length = news.quantity;
         res.render('news', news);
     } else {
-        request(options, getNews);
-            if (news.content.length > 0) {
-                res.render('news', news);
-            } else {
-                res.render('error');
-            }
+        getNews(res);
     }
 });
 
 app.listen(port, () => {
-    console.log(`server listening on port ${port}`)
+    console.log(`server is listening on port ${port}`)
 });
